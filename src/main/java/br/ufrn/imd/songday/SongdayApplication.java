8fcde02bc5ab86298b093d85c513031d90b87f98
Márcio Tenório <marciotenorioj@gmail.com>
2023-06-27 15:04:07 -0300

    register in discovery and have route in gateway

--- src/main/java/br/ufrn/imd/songday/SongdayApplication.java
+++ src/main/java/br/ufrn/imd/songday/SongdayApplication.java
@@ -2,12 +2,14 @@ package br.ufrn.imd.songday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableWebFlux
 @EnableFeignClients
+@EnableDiscoveryClient
 public class SongdayApplication {
 
 	public static void main(String[] args) {
