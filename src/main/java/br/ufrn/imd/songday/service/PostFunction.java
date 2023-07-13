22e7ed6d669708513fa51af022d0249a4dd8c48e
mariaeloi <mariaeduardaeloi@outlook.com>
2023-07-13 10:01:59 -0300

    envia erros para feed

--- src/main/java/br/ufrn/imd/songday/service/PostFunction.java
+++ src/main/java/br/ufrn/imd/songday/service/PostFunction.java
@@ -4,12 +4,14 @@ import java.util.Set;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 import br.ufrn.imd.songday.dto.post.GenerateFeedDto;
 import br.ufrn.imd.songday.dto.post.PostSearchDto;
-import br.ufrn.imd.songday.exception.NotFoundException;
 import br.ufrn.imd.songday.repository.PostRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -22,15 +24,29 @@ public class PostFunction {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private StreamBridge streamBridge;
+
     @Bean
     public Function<Flux<GenerateFeedDto>, Flux<PostSearchDto>> retrievePosts() {
         return input -> {
             return input.flatMap(filter -> {
-                Mono<Set<String>> followeesIdsMono = userService.findFolloweesByUsername(filter.getUsername());
+                Mono<Set<String>> followeesIdsMono = userService.findFolloweesByUsername(filter.getUsername())
+                        .onErrorResume(error -> {
+                            Message<String> event = MessageBuilder.withPayload(error.getMessage()).build();
+                            streamBridge.send("errorsfeed", event);
+                            return Mono.empty();
+                        });
 
                 return followeesIdsMono.flatMapMany(followeesIds -> {
                     return repository.findPosts(followeesIds, filter.getOffset(), filter.getLimit())
-                            .switchIfEmpty(Mono.error(new NotFoundException("Nenhuma publicação encontrada")));
+                            .switchIfEmpty(Mono.defer(() -> {
+                                Message<String> event = MessageBuilder
+                                        .withPayload("Nenhuma publicação encontrada para " + filter.getUsername())
+                                        .build();
+                                streamBridge.send("errorsfeed", event);
+                                return Mono.empty();
+                            }));
                 })
                         .doFirst(() -> {
                             System.out.println("# Buscando posts para o feed de " + filter.getUsername());
